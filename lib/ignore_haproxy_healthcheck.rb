8063b05fdded003bd66d0e6589d6b73663557a92
Jack Chen (chendo) <github@chen.do>
2015-01-22 17:34:59 +1100

    Derp

--- lib/ignore_haproxy_healthcheck.rb
+++ lib/ignore_haproxy_healthcheck.rb
@@ -1,7 +1,8 @@
+require 'ignore_haproxy_healthcheck/middleware'
 module IgnoreHAProxyHealthcheck
   class Railtie < Rails::Railtie
     initializer 'ignore_haproxy_healthcheck.config' do |app|
-      app.config.middleware.insert_before 0, Middleware::IgnoreHAProxyRequests
+      app.config.middleware.insert_before 0, IgnoreHAProxyHealthcheck::Middleware
     end
   end
 end
